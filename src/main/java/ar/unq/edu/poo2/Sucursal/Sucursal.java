a78c851844c3caf772fe0e8a87079f22c8744375
ThomasSandovalUNQ <thomassandovala10@gmail.com>
2026-06-16 10:41:18 -0300

    Sucursal agregada

---
+++ src/main/java/ar/unq/edu/poo2/Sucursal/Sucursal.java
@@ -0,0 +1,51 @@
+package ar.unq.edu.poo2.Sucursal;
+
+import ar.unq.edu.poo2.Catalogo.ItemCatalogo;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Sucursal {
+    private String nombre;
+    // El map guarda cuántas unidades hay de cada ítem en esta sucursal específica
+    private Map<ItemCatalogo, Integer> stockLocal;
+    private List<Sucursal> redGlobal; 
+
+    public Sucursal(String nombre, List<Sucursal> red) {
+        this.nombre = nombre;
+        this.stockLocal = new HashMap<>();
+        this.redGlobal = red;
+    }
+
+    public void registrarStock(ItemCatalogo item, int cantidad) {
+        this.stockLocal.put(item, this.stockLocal.getOrDefault(item, 0) + cantidad);
+    }
+
+    public boolean tieneStockLocal(ItemCatalogo item, int cantidadNecesaria) {
+        return this.stockLocal.getOrDefault(item, 0) >= cantidadNecesaria;
+    }
+
+    public void decrementarStock(ItemCatalogo item, int cantidad) {
+        if (!tieneStockLocal(item, cantidad)) {
+            throw new RuntimeException("Stock insuficiente en la sucursal: " + this.nombre);
+        }
+        this.stockLocal.put(item, this.stockLocal.get(item) - cantidad);
+    }
+
+    public void incrementarStock(ItemCatalogo item, int cantidad) {
+        registrarStock(item, cantidad);
+    }
+    
+    
+    public int estimarDiasDeRetiro(ItemCatalogo item, int cantidad) {
+        if (this.tieneStockLocal(item, cantidad)) {
+            return 0;
+        } 
+        else if (this.redGlobal.stream().anyMatch(s -> s.tieneStockLocal(item, cantidad))) {
+            return 3;
+        } 
+        else {
+            throw new RuntimeException("No hay stock en ninguna sucursal del país.");
+        }
+    }
+}
